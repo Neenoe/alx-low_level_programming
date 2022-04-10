9a195f22c078233712e2594367e3a2b1b2f64e10
Neenoe <adavaoluwatobisamuel@gmail.com>
2022-04-10 16:04:43 -0700

    Hello world

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,24 +6,16 @@
  *
  *  * main - prints the alphabet in lowercase, and then in uppercase,
  *
- *   * followed by a new line
+ *   followed by a new line
  *
  *    * Return: Always 0 (Success)
  *
  *     */
-
-int main(void)
-	
-{
-	
+int main(void)	
+{	
 int ch;
-		
-		
-for (ch = 'a'; ch <= 'z'; ch++)
-				
-putchar(ch);
-				
-putchar('\n');
-						
+for (ch = 'a'; ch <= 'z'; ch++)			
+putchar(ch);			
+putchar('\n');						
 return (0);
 }
