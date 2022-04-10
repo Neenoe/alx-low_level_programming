608031550e0c4b4e143e42ed631303cf28dd1218
Neenoe <adavaoluwatobisamuel@gmail.com>
2022-04-10 16:14:09 -0700

    God abegg

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -11,11 +11,11 @@
  *    * Return: Always 0 (Success)
  *
  */
-int main(void)	
-{	
+int main(void)
+{
 int ch;
-for (ch = 'a'; ch <= 'z'; ch++)		
-putchar(ch);			
-putchar('\n');						
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+putchar('\n');
 return (0);
 }
