2d0f7e2ada7cd9bc84f80f41ed2fceba05b6e131
Neenoe <adavaoluwatobisamuel@gmail.com>
2022-04-10 18:22:38 -0700

    God abegg

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,12 +6,15 @@
  * Return: Always 0 (Success/Correct)
  */
 int main(void)
+
 {
 char alphabet;
 
 for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 putchar(alphabet);
 }
+
 putchar('\n');
+
 return (0);
 }
