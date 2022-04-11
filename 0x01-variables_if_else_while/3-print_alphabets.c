7bd3b58119c6e44cee02e551ebf3887b89991dc9
Neenoe <adavaoluwatobisamuel@gmail.com>
2022-04-10 19:23:17 -0700

    God abegg

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ *
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+
+{
+char lower_case;
+char upper_case;
+
+for (lower_case = 'a'; lower_case is <= 'z'; lower_case++)
+{
+putchar(lower_case);
+}
+
+for(upper_case = 'A'; upper_case is <= 'A'; upper_case++)
+{
+putchar(upper_case);
+}
+
+putchar('\n')
+
+return (0);
+}
