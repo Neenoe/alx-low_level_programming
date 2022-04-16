420bff460bf9ab483fe3a233f1a8d5a4877a2de4
Neenoe <adavaoluwatobisamuel@gmail.com>
2022-04-15 18:22:23 -0700

    God abegg

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,14 +8,11 @@
  */
 void print_square(int size)
 {
-	if (size <= 0)
-	{
-		_putchar('\n');
-	} else
-	{
-		int i, j;
+	int i, j;
 
-		for (i = 0, i < size; i++)
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
 		{
 			for (j = 0; j < size; j++)
 			{
@@ -23,5 +20,9 @@ void print_square(int size)
 			}
 			_putchar('\n');
 		}
+	}
+	else
+	{
+		_putchar('\n');
 	}
 }
