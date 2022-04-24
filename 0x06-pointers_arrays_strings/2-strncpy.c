dd5910e2a0dc20bb0225949d545ddf421dfdd5ad
Neenoe <adavaoluwatobisamuel@gmail.com>
2022-04-24 11:59:03 -0700

    My Commit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _strncpy - copies a string
  * @dest: destination.
@@ -12,9 +13,9 @@ char *_strncpy(char *dest, char *src, int n)
 	int i;
 
 	for (i = 0; i < n && src[i] != '\0';
-			dest[i] = src[i];
-			for ( ; i < n; i++)
-			dest[i] = '\0';
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
 
-			return (dest);
+	return (dest);
 }
