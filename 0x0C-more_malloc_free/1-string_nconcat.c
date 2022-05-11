3709a7c1a0fd7d791e0626895aff966c3cd5c37b
Neenoe <adavaoluwatobisamuel@gmail.com>
2022-05-10 17:06:29 -0700

    My Commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -35,7 +35,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	sout = malloc(lsout + 1);
 
 	if (sout == NULL)
-	        return (NULL);
+	return (NULL);
 
 	for (i = 0; i < lsout; i++)
 		if (i < ls1)
